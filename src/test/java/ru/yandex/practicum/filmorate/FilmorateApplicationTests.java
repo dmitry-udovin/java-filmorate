1a98f8ac0588ef9fb8c8f75fc907e990a6938e13
Dmitry Vdovin <dimav0406@gmail.com>
2026-02-20 16:30:33 +0300

    fix #5 for github, file name

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -18,7 +18,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
-class FilmoRateApplicationTests {
+class FilmorateApplicationTests {
 
     private final DbUserStorage userStorage;
     private final JdbcTemplate jdbcTemplate;
