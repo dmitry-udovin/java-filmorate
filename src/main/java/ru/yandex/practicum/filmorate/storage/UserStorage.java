158ae62d8bfae3f6454bd4b77aa6fdf16bd1b906
Dmitry Vdovin <dimav0406@gmail.com>
2026-02-20 14:44:59 +0300

    fix #4 for github, line separator

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -6,15 +6,24 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserStorage {
+
     Optional<User> getUserFromStorage(long userId);
+
     User addUserInStorage(User user);
+
     User updateUserInStorage(User user);
+
     List<User> getAllUsersFromStorage();
+
     void deleteUserFromStorage(Long userId);
 
     // Методы для работы с друзьями
     void addFriend(long userId, long friendId);
+
     void deleteFriend(long userId, long friendId);
+
     List<User> getFriends(long userId);
+
     List<User> getCommonFriends(long userId, long otherId);
+
 }
